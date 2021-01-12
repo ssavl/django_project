9d692b49f9ee2d0c2a8ca1155c8d25036500b8c3
ssavl <get-moscow@yandex.ru>
2021-01-12 23:43:45 +0300

    another commit

---
+++ django_project/config.py
@@ -0,0 +1,3 @@
+SECRET_KEY = '(3z8-rsj#3(hnm=biupctsn8o57n3$l4(s#5$opi)ge-7fk%!*'
+
+client = MongoClient('mongodb+srv://megauser:YNiTjy5QarJBO3XN@cluster0.h4sze.mongodb.net/<dbname>?retryWrites=true&w=majority')
