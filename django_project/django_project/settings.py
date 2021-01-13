728d1f122319a95d2ef94fb36c9a2af09e807312
ssavl <get-moscow@yandex.ru>
2021-01-13 14:36:54 +0300

    added secure

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
-
+from config import SECRET_KEY
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -19,7 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(3z8-rsj#3(hnm=biupctsn8o57n3$l4(s#5$opi)ge-7fk%!*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
